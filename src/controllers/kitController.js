d29b1b03fdda1ab8262388a3d282eac63e08b860
Melissa Belkessam <melissabelkessmabelkessam@gmail.com>
2025-06-27 00:09:14 +0200

    modif de kitcontroller

--- src/controllers/kitController.js
+++ src/controllers/kitController.js
@@ -5,7 +5,7 @@ class KitController {
   async create(req, res) {
   try {
     const { name, description, price, stock, category } = req.body;
-    const image = req.file ? req.file.path : null; 
+    const image = req.file ? `uploads/kits/${req.file.filename}` : null;
 
     if (!name || !description || !price || !image) {
       return res.status(400).json({ error: "Nom, description, prix et image sont requis." });
