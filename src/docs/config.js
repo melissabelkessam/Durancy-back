e66f6c892527d775151b559563ee4561c045c5e2
Melissa Belkessam <melissabelkessmabelkessam@gmail.com>
2025-07-24 16:26:12 +0200

    modif de swwager

--- src/docs/config.js
+++ src/docs/config.js
@@ -4,7 +4,7 @@ const options = {
     definition: {
         openapi: "3.1.0",
         info: {
-            title: "DurancyApi",
+            title: "DurancyMelissa",
             version: "0.1.0",
             description:
                 "This is a simple CRUD API application made with Express and documented with Swagger",
